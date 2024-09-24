c64fdfc830330f3ecacedbd97edeb2b22571d6e8
YingqiuLi <li.yingq@northeastern.edu>
2024-09-24 19:44:11 -0400

    fourth commit

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -163,7 +163,7 @@ export default function Lab1() {
                 <br />
                 Loading a local image:
                 <br />
-                <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" alt="wd-testabot"/>
+                <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" alt="wd-teslabot"/>
             </div>
             <div id="wd-forms">
                 <h4>Form Elements</h4>
